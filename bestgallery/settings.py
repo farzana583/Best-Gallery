2c95e39205e3ce38c77f9903616eb5c072e3bceb
farzana583 <farzanaisack@gmail.com>
2021-01-14 22:35:16 +0300

    add

--- bestgallery/settings.py
+++ bestgallery/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
+    'bootstrap3',
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
